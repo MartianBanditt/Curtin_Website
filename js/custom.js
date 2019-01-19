5c6e560a811404ab9efa0857c2add12d859f23a1
s3506272 <s3506272@student.rmit.edu.au>
2019-01-19 17:19:07 +1100

    Add custom scrips file, date picker range

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+// Restricts date fields to today or the future
+// Adapted from here https://stackoverflow.com/questions/43274559/how-do-i-restrict-past-dates-in-html5-input-type-date
+$(function() {
+
+  $(document).ready(function () {
+
+   var currentDate = new Date();
+
+    // Max date attribute is in "YYYY-MM-DD".  Need to format today's date accordingly
+
+    var month = ("0" + (currentDate.getMonth() + 1)).slice(-2);  // MM
+
+    var day = ("0" + currentDate.getDate()).slice(-2); //slice needed for single digit date
+
+
+    var currentDateFormated = (currentDate.getFullYear() + "-" + month + "-" + day);
+
+     
+    // Now to set the max date value for the calendar to be today's date
+
+    $('.form-control.booking-date').attr('max',currentDateFormated);
+
+  });
+
+});
