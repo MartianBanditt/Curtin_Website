d093314d1648b7470deaee1d7b417bd5a32d4c3e
MartianBanditt <jasonaelane@gmail.com>
2019-01-26 15:26:17 +1100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!-- Move this to a function file -->
 <?php
    require_once 'scripts/functions.php';
 ?>
@@ -27,10 +28,10 @@
     <main>
         <!-- Call to aciton section, funnels through to questionaires -->
         <div class="jumbotron mb-5" id="home-bg-img">
-            <div class="container">
+            <div class="container p-0 p-sm-3">
                 <section id="cta" class="col-sm-12 col-md-5 offset-md-0 bg-panel-b p-sm-5 p-4">
 
-                    <h2>We are more than just a Bank</h2>
+                    <h2>We are more than a Bank</h2>
                     <p class="my-4">We're insurance, technology, investment and banking.</p>
                     <a href="find-a-service.php" class="btn btn-primary btn-lg">Help us find you the right service</a>
 
@@ -44,6 +45,39 @@
                 <div class="row" id="front-help">
 
                     <div class="col mb-sm-0 mb-3">
+
+                        <div class="card mb-sm-4 mb-3">
+                            <div class="card-header bg-panel-b text-white">
+                                <h3 class="card-title mt-2 ml-3"><span><i class="far fa-credit-card mr-2"></i></span> Our products</h3>
+                            </div>
+                            <div class="card-body">
+                                <ul class="px-3">
+                                    <li class="py-3"><a href="card-types.php#credit-cards" title="Credit Cards">Credit cards</a></li>
+                                    <li class="py-3"><a href="card-types.php#debit-cards" title="Debit Card">Debit Cards</a></li>
+                                    <li class="pt-3"><a href="card-types.php#travel-money-cards" title="Travel money Card">Travel money cards</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-12 p-3 p-sm-5 no-gutters bg-third mb-sm-0 mb-0">
+                            <div class="row">
+                                <div class="col-4">
+                                    <span class="fa-large-front"><i class="fas fa-mobile-alt"></i></span>
+                                    <span class="fa-large-front mt-sm-3"><i class="fab fa-bitcoin"></i></span>
+
+                                </div>
+                                <div class="col">
+                                    <h3>Financial Tech</h3>
+                                    <p>Mobile pay, cryptocurrency, smart-tech, learn more about how we harness fintech to enhance your banking experience</p>
+                                    <a href="fin-tech.php" class="btn btn-primary btn-lg">Find out more</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col mb-sm-0 mb-3">
+
                         <div class="bg-third p-sm-5 p-4">
                             <div class="">
                                 <h2>Need help understanding your options?</h2>
@@ -53,17 +87,8 @@
                                     <li>Index funds</li>
                                     <li>Credit types</li>
                                 </ul>
-                                <a href="services.php" class="btn btn-primary btn-lg">Find out more</a>
+                                <a href="faq.php" class="btn btn-primary btn-lg">Find out more</a>
                             </div>
-                        </div>
-                    </div>
-
-                    <div class="col">
-                        <div class="mb-sm-5 mb-3 p-sm-5 p-4">
-                            <h3>Technology</h3>
-                        </div>
-                        <div class="p-sm-5 p-4">
-                            <h3>Security</h3>
                         </div>
                     </div>
 
@@ -71,13 +96,13 @@
             </div>
         </div>
 
-        <div class="full-width-background my-sm-5 my-3">
+        <div class="full-width-background my-sm-5 mb-3">
             <div class="container">
                 <div class="row no-gutters">
                     <div class="col-md-8 p-sm-5 p-4 bg-third">
                         <h2>We help people every day</h2>
                         <p>Find out how we help them achieve their goals, manage their finances, and make decisions that work for them.</p>
-                        <a href="services.php" class="btn btn-primary btn-lg">Find out more</a>
+                        <a href="about-us.php" class="btn btn-primary btn-lg">Find out more</a>
                     </div>
                     <div class="col-sm-12 col-md-4">
                         <img src="media/handsome-man-coffee-flipped.png" class="img-fluid" alt="...">
