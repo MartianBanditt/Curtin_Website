65d8e2cb64c651c50b4e2e75b57bb4c4ae55f643
s3506272 <s3506272@student.rmit.edu.au>
2019-02-20 19:52:03 +1100

    Update hero section, Change questionaire

--- find-a-service.php
+++ find-a-service.php
@@ -33,10 +33,10 @@
         </div>
 
 
-        <div class="jumbotron mb-5" id="card-hero">
+        <div class="jumbotron" id="card-hero">
             <div class="container">
                 <div class="row no-gutters">
-                    <div class="col-sm-12 col-md-7 bg-white offset-md-0 p-sm-5 p-4">
+                    <div class="col-sm-12 col-md-7 offset-md-0 p-sm-5 p-4">
                         <h2>Finding the right product or service can be tricky</h2>
                         <p>That's why we've made it easy by allowing you to tell us what matters most to you and how you want to use our services.</p>
                         <p>Pick from the options below and you'll match with the right product.</p>
@@ -44,70 +44,77 @@
                 </div>
             </div>
         </div>
+        <div class="full-width-background py-5 bg-third">
+            <div class="container">
+                <form class="row p-3">
+                    <div class="col-2 pr-2 p-0 py-2">
+                        <label for="card-type-dc">I'm looking for a?</label>
+                        <select id="card-type-dc" name="card-type-dc">
+                            <option value="select">Select</option>
 
-        <div class="row justify-content-center p-3">
-            <div class="col-3 p-3">
-                <label for="card-type-dc">I'm looking for a?</label>
-                <select id="card-type-dc" name="card-type-dc">
-                    <option value="select">Select</option>
+                            <option value="credit">Credit card</option>
 
-                    <option value="credit">Credit card</option>
+                            <option value="debit">Debit card</option>
 
-                    <option value="debit">Debit card</option>
+                        </select>
+                    </div>
+                    <div class="col-2 p-2">
+                        <label for="card-type-interest">I'm interested in?</label>
+                        <select id="card-type-interest" name="card-type-interest">
 
-                </select>
+                            <option value="select">Select</option>
+
+                            <option value="rewards-types">Rewards and perks</option>
+                            <option value="saving">Saving money</option>
+                            <option value="ease">Ease of use</option>
+
+                        </select>
+                    </div>
+
+                    <!-- Question layer 2 reward types -->
+                    <div class="col-2 p-2" id="reward-types">
+                        <label for="card-type-reward-types">Reward types?</label>
+                        <select id="card-type-reward-types" name="card-type-reward-types">
+
+                            <option value="select">Select</option>
+
+                            <option value="cash-back">Cash Back</option>
+                            <option value="travel">Travel</option>
+                            <option value="flexible">Flexible</option>
+
+                        </select>
+                    </div>
+
+                    <!-- Question layer 2 saving money -->
+                    <div class="col-2 p-2" id="saving">
+                        <label for="card-type-saving">Saving money</label>
+                        <select id="card-type-saving" name="card-type-saving">
+
+                            <option value="select">Select</option>
+                            <option value="high-interest">High interest</option>
+                            <option value="low-fees">Low Account fees</option>
+
+                        </select>
+                    </div>
+
+                    <!-- Question layer 2 reward types -->
+                    <div class="col-2 p-2" id="ease">
+                        <label for="card-type-ease">I'll use this for</label>
+                        <select id="card-type-ease" name="card-type-ease">
+
+                            <option value="select">Select</option>
+                            <option value="online">Shopping online</option>
+                            <option value="instore">Shopping instore</option>
+
+                        </select>
+                    </div>
+                <div class="col-2 submit pt-4 align-item-bottom">
+                    <input class="btn btn-primary btn-lg" type="submit" name="submit-card" value="Submit">
+                </div>
+                </form>
             </div>
-            <div class="col-3 justify-content-center p-3">
-                <label for="card-type-dc">I'm interested in?</label>
-                <select id="card-type-dc" name="card-type-dc">
-                    <option value="select">Select</option>
-
-                    <option value="rewards-perks">rewards and perks</option>
-
-                    <option value="saving">Saving money</option>
-                    <option value="ease">Ease of use</option>
-
-                </select>
-            </div>
-
-
         </div>
 
-        <div class="full-width-background my-5 py-5 bg-third">
-            <div class="container accordion" id="accordionExample">
-                <!-- Question for user -->
-                <div class="row text-center bg-dark text-light p-2 m-auto question-box">
-                    <h2 class=""><a href="#collapseOne" class="text-white" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Hi there! What type of card are you looking for today?</a></h2>
-                </div>
-
-                <div class="row justify-content-center p-3 flex-width collapse show answer-box" id="collapseOne" aria-labelledby="headingOne" data-parent="#accordionExample">
-                    <div class="col m-auto">
-                        <button type="button" class="btn btn-circle" role="button" data-toggle="collapse" data-parent="#accordionExample" href="#collapseTwo" aria-expanded="false">Credit Card</button>
-                    </div>
-                    <div class="col m-auto">
-                        <button type="button" class="btn btn-circle" role="button" data-toggle="collapse" data-parent="#accordionExample" href="#collapseTwo" aria-expanded="false">Debit Card</button>
-                    </div>
-                </div>
-
-                <!-- Question for user -->
-                <div class="row text-center bg-dark text-light p-2 m-auto question-box">
-                    <h2 class=""><a href="#collapseTwo" id="headingTwo" data-toggle="collapse" class="text-white collapsed" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">What are you most interested in?</a></h2>
-                </div>
-
-                <div class="row justify-content-center p-3 flex-width collapse answer-box" id="collapseTwo" aria-labelledby="headingTwo" aria-expanded="false" data-parent="#accordionExample">
-                    <div class="col m-auto">
-                        <button type="button" class="btn btn-circle">Rewards and perks</button>
-                    </div>
-                    <div class="col m-auto">
-                        <button type="button" class="btn btn-circle">Saving money</button>
-                    </div>
-                    <div class="col m-auto">
-                        <button type="button" class="btn btn-circle">Ease of use</button>
-                    </div>
-                </div>
-            </div>
-
-        </div>
 
 
         <div class="full-width-background my-5">
