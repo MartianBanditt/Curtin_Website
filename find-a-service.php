a38f95c5a4baef56009963b0a276de51c95daeed
s3506272 <s3506272@student.rmit.edu.au>
2019-02-20 18:21:03 +1100

    Change top image into hero image

--- find-a-service.php
+++ find-a-service.php
@@ -26,37 +26,61 @@
 
     <main>
         <!-- Call to aciton section, funnels through to questionaires -->
-        <div class="full-width-background title mb-5">
+        <div class="full-width-background title">
             <section id="title" class="container">
-
                 <h1>Find a product or service</h1>
             </section>
         </div>
 
 
-        <div class="full-width-background my-5">
+        <div class="jumbotron mb-5" id="card-hero">
             <div class="container">
                 <div class="row no-gutters">
-                    <div class="col-md-8 p-5 bg-third">
+                    <div class="col-sm-12 col-md-7 bg-white offset-md-0 p-sm-5 p-4">
                         <h2>Finding the right product or service can be tricky</h2>
                         <p>That's why we've made it easy by allowing you to tell us what matters most to you and how you want to use our services.</p>
                         <p>Pick from the options below and you'll match with the right product.</p>
-                    </div>
-                    <div class="col-sm-12 col-md-4">
-                        <img src="media/handsome-man-coffee-flipped.png" class="img-fluid" alt="Stock photo of handsome person, to be replaced">
                     </div>
                 </div>
             </div>
         </div>
 
+        <div class="row justify-content-center p-3">
+            <div class="col-3 p-3">
+                <label for="card-type-dc">I'm looking for a?</label>
+                <select id="card-type-dc" name="card-type-dc">
+                    <option value="select">Select</option>
+
+                    <option value="credit">Credit card</option>
+
+                    <option value="debit">Debit card</option>
+
+                </select>
+            </div>
+            <div class="col-3 justify-content-center p-3">
+                <label for="card-type-dc">I'm interested in?</label>
+                <select id="card-type-dc" name="card-type-dc">
+                    <option value="select">Select</option>
+
+                    <option value="rewards-perks">rewards and perks</option>
+
+                    <option value="saving">Saving money</option>
+                    <option value="ease">Ease of use</option>
+
+                </select>
+            </div>
+
+
+        </div>
+
         <div class="full-width-background my-5 py-5 bg-third">
             <div class="container accordion" id="accordionExample">
                 <!-- Question for user -->
-                <div class="row">
-                    <h2 class="text-center bg-dark text-light p-3 d-inline-block m-auto q-radius"><a href="#collapseOne" class="text-white" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Hi there! What type of card are you looking for today?</a></h2>
+                <div class="row text-center bg-dark text-light p-2 m-auto question-box">
+                    <h2 class=""><a href="#collapseOne" class="text-white" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Hi there! What type of card are you looking for today?</a></h2>
                 </div>
 
-                <div class="row justify-content-center mt-5 flex-width collapse show" id="collapseOne" aria-labelledby="headingOne" data-parent="#accordionExample">
+                <div class="row justify-content-center p-3 flex-width collapse show answer-box" id="collapseOne" aria-labelledby="headingOne" data-parent="#accordionExample">
                     <div class="col m-auto">
                         <button type="button" class="btn btn-circle" role="button" data-toggle="collapse" data-parent="#accordionExample" href="#collapseTwo" aria-expanded="false">Credit Card</button>
                     </div>
@@ -66,11 +90,11 @@
                 </div>
 
                 <!-- Question for user -->
-                <div class="row mt-5">
-                    <h2 class="text-center bg-dark text-light p-3 d-inline-block m-auto q-radius"><a href="#collapseTwo" id="headingTwo" data-toggle="collapse" class="text-white collapsed" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">What are you most interested in?</a></h2>
+                <div class="row text-center bg-dark text-light p-2 m-auto question-box">
+                    <h2 class=""><a href="#collapseTwo" id="headingTwo" data-toggle="collapse" class="text-white collapsed" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">What are you most interested in?</a></h2>
                 </div>
 
-                <div class="row justify-content-center mt-5 flex-width collapse" id="collapseTwo" aria-labelledby="headingTwo" aria-expanded="false" data-parent="#accordionExample">
+                <div class="row justify-content-center p-3 flex-width collapse answer-box" id="collapseTwo" aria-labelledby="headingTwo" aria-expanded="false" data-parent="#accordionExample">
                     <div class="col m-auto">
                         <button type="button" class="btn btn-circle">Rewards and perks</button>
                     </div>
