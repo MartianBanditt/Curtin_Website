d4c1ffda8afdcd02e65442ecb3fa18fbf47694d7
s3506272 <s3506272@student.rmit.edu.au>
2019-02-23 18:03:49 +1100

    Remove uneeded date function, Fix select on click with no value

--- js/custom.js
+++ js/custom.js
@@ -1,44 +1,18 @@
-// Restricts date fields to today or the future
-// Adapted from here https://stackoverflow.com/questions/43274559/how-do-i-restrict-past-dates-in-html5-input-type-date
-$(function() {
-
-  $(document).ready(function () {
-
-   var currentDate = new Date();
-
-    // Max date attribute is in "YYYY-MM-DD".  Need to format today's date accordingly
-
-    var month = ("0" + (currentDate.getMonth() + 1)).slice(-2);  // MM
-
-    var day = ("0" + currentDate.getDate()).slice(-2); //slice needed for single digit date
-
-
-    var currentDateFormated = (currentDate.getFullYear() + "-" + month + "-" + day);
-
-     
-    // Now to set the max date value for the calendar to be today's date
-
-    $('.form-control.booking-date').attr('max',currentDateFormated);
-
-  });
-
-});
-
-
+// Hides fields in case js is disabled they will be shown
 $('.layer-2').addClass('dont-show');
 $('.layer-3').addClass('dont-show');
 
+// Toggles the dont-show class based on form select values
 $("#card-type-interest").change(function(){
-     console.log( this.value );
     $('.layer-2').addClass('dont-show');
     $('.layer-3').addClass('dont-show');
-   $('#'+this.value).toggleClass('dont-show');
-
+    if (this.value !=''){
+   $    ('#'+this.value).toggleClass('dont-show');
+    }
 });
 
+// Toggles the dont-show class based on form select values
 $(".layer-2 select").change(function(){
-     console.log( this.value );
-    $('.layer-3').addClass('dont-show');
     $('.layer-3').addClass('dont-show');
    $('#'+this.value).toggleClass('dont-show');
 });
